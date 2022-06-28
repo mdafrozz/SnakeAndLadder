3060824be57d6a72ba89c79cc711830300261fe6
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-28 20:22:52 +0530

    Refactor: Player checks for a Option
    The Player then checks for
    a Option. They are No Play,
    Ladder or Snake.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,12 +3,29 @@ package snake_ladder;
 public class SnakeAndLadder {
 	
 	public static final int START_POSITION = 0;// UC1 - Start position 0
-
+	public static final int IS_SNAKE = 1;
+	public static final int IS_LADDER = 2;
+	public static final int NO_PLAY = 0;
+	
+	public static int currentPosition = 0;
 
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
 		System.out.println("Player start Position is: "+START_POSITION);
+		
 		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
 		System.out.println("Dice Number : " +diceNumber);
+		
+		int option = (int) ((Math.random() * 10 % 3) + 1); // UC3 - Player check options
+		if (option == IS_SNAKE) {
+			currentPosition -= diceNumber;
+			System.out.println("Snake bites....");
+		} else if (option == IS_LADDER) {
+			currentPosition += diceNumber;
+			
+		} else if (option == NO_PLAY) {
+			System.out.println("NO Play");
+		}
+
 	}
 }
