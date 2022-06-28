f648900ef52e7ff923e57e60a1986935833fa50c
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-28 20:18:04 +0530

    Refactor: Roll die to get a number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,11 +2,13 @@ package snake_ladder;
 
 public class SnakeAndLadder {
 	
-	public static final int START_POSITION = 0;
-	
+	public static final int START_POSITION = 0;// UC1 - Start position 0
+
 
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
 		System.out.println("Player start Position is: "+START_POSITION);
+		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
+		System.out.println("Dice Number : " +diceNumber);
 	}
 }
